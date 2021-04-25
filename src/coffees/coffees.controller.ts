9a8805523f9c006d8e0528dd204e07e063d7cd44
Juan Felipe Salcedo <jsalcedo@leal.co>
2021-04-25 18:55:02 -0500

    Chapter 6 completed

--- src/coffees/coffees.controller.ts
+++ src/coffees/coffees.controller.ts
@@ -1,23 +1,28 @@
-import { Body, Controller, Delete, Get, Param, Patch, Post, Query } from '@nestjs/common';
-import { PaginationQueryDto } from 'src/common/dto/pagination-query.dto';
+import { Body, Controller, Delete, Get, Param, Patch, Post, Query, SetMetadata } from '@nestjs/common';
+import { ParseIntPipe } from '../common/pipes/parse-int.pipe';
+import { Public } from '../common/decorators/public.decorator';
+import { PaginationQueryDto } from '../common/dto/pagination-query.dto';
 import { CoffeesService } from './coffees.service';
 import { CreateCoffeeDto } from './dto/create-coffee.dto';
 import { UpdateCoffeeDto } from './dto/update-coffee.dto';
+import { Protocol } from 'src/common/decorators/protocol.decorator';
 
 @Controller('coffees')
 export class CoffeesController {
 
     constructor(private readonly coffeesService: CoffeesService) {}
 
+    @Public()
     @Get()
-    findAll(@Query() paginationQuery: PaginationQueryDto) {
-        // const { limit, offset } = paginationQuery;
+    async findAll(@Protocol('https') protocol: string, @Query() paginationQuery: PaginationQueryDto) {
+        console.log(protocol);
         return this.coffeesService.findAll(paginationQuery);
     }
 
     @Get(':id')
-    findOne(@Param('id') id: string) {
-        return this.coffeesService.findOne(id);
+    findOne(@Param('id', ParseIntPipe) id: number) {
+        console.log(id);
+        return this.coffeesService.findOne('' + id);
     }
 
     @Post()

271d5e7130ccacb2c58d58e2dd9f0e286ff9c6da
Juan Felipe Salcedo <jsalcedo@leal.co>
2021-04-25 19:16:49 -0500

    Swagger added

--- src/coffees/coffees.controller.ts
+++ src/coffees/coffees.controller.ts
@@ -6,12 +6,15 @@ import { CoffeesService } from './coffees.service';
 import { CreateCoffeeDto } from './dto/create-coffee.dto';
 import { UpdateCoffeeDto } from './dto/update-coffee.dto';
 import { Protocol } from 'src/common/decorators/protocol.decorator';
+import { ApiForbiddenResponse, ApiTags } from '@nestjs/swagger';
 
+@ApiTags('coffees')
 @Controller('coffees')
 export class CoffeesController {
 
     constructor(private readonly coffeesService: CoffeesService) {}
 
+    @ApiForbiddenResponse({ description: 'Forbidden.'})
     @Public()
     @Get()
     async findAll(@Protocol('https') protocol: string, @Query() paginationQuery: PaginationQueryDto) {
